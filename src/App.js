9e137689df404836a6c4d792fa341948adec9400
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-08 10:18:10 +0300

    API fetch function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [survey, setSurvey] = useState({});
+  const [ready, setReady] = useState(false);
+
+  // API query options
+  const API_URL = '';
+
+  // Call API
+  const getQuestions = () => {
+    fetch(API_URL)
+      .then(response => {
+        if (!response.ok) throw new Error(response.statusText);
+
+        return Response.json();
+      })
+      .then(responseData => {
+        setSurvey(responseData);
+        setReady(true);
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div >
-     Olen kyselypalvelu.
+      Olen kyselypalvelu.
     </div>
   );
 }
