528d51dc17c3e6d1a0176a999ac56e2e0f613cd1
Tuomas Valkamo <valkamo.tuomas@gmail.com>
2021-04-13 14:44:49 +0300

    renamed state

--- src/components/surveys.js
+++ src/components/surveys.js
@@ -1,83 +1,70 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 
 function Surveys() {
+  const [survey, setSurvey] = useState([{
+    name: '',
+    amount: '',
+    status: '',
+    questions: [],
+    id: ''
+  }]);
 
+  const [answer, setAnswer] = useState('');
+  const [allanswers, setAllanswers] = useState([]);
 
-    const [fetchedsurvey, setFetchedsurveys] = useState([{
-        name: '',
-        amount: '',
-        status: '',
-        questions: [],
-        id: ''
-    }]);
-     
-const [answer, setAnswer] = useState('');
-const [allanswers, setAllanswers] = useState([]);
+  const url = 'https://survey-task.herokuapp.com/surveys';
 
-const url = 'https://survey-task.herokuapp.com/surveys';
+  const fetch_url = async () => {
+    try {
+      const response = await fetch(url)
+      const json = await response.json();
 
-const fetch_url = async () => {
+      console.log(json)
 
-    try{
-
-
-const response = await fetch(url)
-const json = await response.json();
-
-console.log(json)
-
-setFetchedsurveys(json);
+      setSurvey(json);
     }
-    catch{
-console.log('oops!')
+    catch {
+      console.log('Error while fetching surveys.')
     }
 
-}
+  }
 
-const returnAnswer = (e) => {
+  const returnAnswer = (e) => {
+    setAnswer(e.target.value);
+  }
 
-setAnswer(e.target.value);
-
-}
-
-const giveAnswer = (e) => {
-
+  const giveAnswer = (e) => {
     e.preventDefault();
     setAllanswers(allanswers.push(answer));
     console.log(allanswers)
     setAnswer('');
-}
+  }
 
-    return(
-        <div>
-
-           <button onClick = {() =>fetch_url()} >Hae surveys</button>
-           <div>
-        {fetchedsurvey.map(e => {
-            return(
-              <p key = {e.id}>
-                amount: {e.amount}<br></br>
+  return (
+    <div>
+      <button onClick={() => fetch_url()} >Hae surveys</button>
+      <div>
+        {survey.map(e => {
+          return (
+            <p key={e.id}>
+              amount: {e.amount}<br></br>
                 id: {e.id}<br></br>
                 name: {e.name}<br></br>
                 questions : {e.questions}<br></br>
                 status:{ e.status}<br></br>
-                </p>
-                 
-            );
-        })}
-    </div>
+            </p>
 
-            
-    <form>
-<label htmlFor = 'answer'>Answer to Question </label>
-<input type = 'text' name= 'answer' value = {answer} onChange= {(e) =>returnAnswer(e)}/> <br></br>
-<button onClick = {(e) =>giveAnswer(e)}>Give answer</button>
-   </form>
-        </div>
-    )
+          );
+        })}
+      </div>
+
+      <form>
+        <label htmlFor='answer'>Answer to Question </label>
+        <input type='text' name='answer' value={answer} onChange={(e) => returnAnswer(e)} /> <br></br>
+        <button onClick={(e) => giveAnswer(e)}>Give answer</button>
+      </form>
+    </div>
+  )
 }
 
-
 export default Surveys;
-
