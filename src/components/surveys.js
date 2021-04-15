b639984c3ef4acf36479083a9445ed66ea846cce
AnneMmannila <70891200+AnneMmannila@users.noreply.github.com>
2021-04-15 10:56:06 +0300

    commit

--- src/components/surveys.js
+++ src/components/surveys.js
@@ -21,7 +21,7 @@ function Surveys() {
 
       console.log(json)
       setSurvey(json);
-      console.log(survey[0].questions);
+     
     }
     catch {
       console.log('Error while fetching surveys.')
@@ -44,30 +44,7 @@ function Surveys() {
     <div>
       <button onClick={() => fetch_url()} >Hae surveys</button>
       <div>
-<<<<<<< HEAD
        
-=======
-        {survey.map(e => {
-          return (
-            <div key={e.id}>
-              amount: {e.amount}<br></br>
-                id: {e.id}<br></br>
-                name: {e.name}<br></br>
-                questions :
-              {e.questions.map(q => {
-                return (
-                  <div key={q.questionId}>
-                    <p>Question id: {q.questionId}</p>
-                    <p>Question type: {q.type}</p>
-                    <p>Question text: {q.quetext}</p>
-                    <p>Question required?: {q.required}</p>
-                  </div>
-                );
-              })}
-              <br></br>
-                status:{ e.status}<br></br>
-            </div>
->>>>>>> 7789e07bf8f3aa9420697c3d8bfdc4b0ee247fca
 
       </div>
 
